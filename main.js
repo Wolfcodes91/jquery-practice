1efbfca66d2f353960da35805157e4dd8bb75fc4
Wolfcodes91 <Dave.M.Wolfberg@gmail.com>
2022-11-07 14:19:39 -0800

    favorite pokemon list using ajax

--- main.js
+++ main.js
@@ -20,26 +20,26 @@ $(() => {
         }
     })
 
-    //setInterval 
-
     // configuration
     var width = 720;
     var animationSpeed = 1000;
     var pause = 3000;
     var currentSlide = 1
-    
+
     //cache DOM
     var $slider = $('#slider');
     var $slideCotainer = $slider.find('.slides');
     var $slides = $slideCotainer.find('.slide');
-
+    //setInterval
     var interval;
 
+    // animate margin-left
     function startSlider() {
-        interval = setInterval(function () {
-            $slideCotainer.animate({ 'margin-left': `-=${width}`}, animationSpeed, function () {
-                currentSlide ++;
-                if(currentSlide === $slides.length) {
+        interval = setInterval(() => {
+            $slideCotainer.animate({ 'margin-left': `-=${width}` }, animationSpeed, function () {
+                currentSlide++;
+                // if last slide, go to position 1 (0px)
+                if (currentSlide === $slides.length) {
                     currentSlide = 1;
                     $slideCotainer.css('margin-left', 0)
                 }
@@ -50,12 +50,28 @@ $(() => {
     function pauseSlider() {
         clearInterval(interval)
     }
-    $slider.on('mouseenter', pauseSlider).on('mouseleave', startSlider )
-
-    startSlider();
-    // animate margin-left
-    // if last slide, go to position 1 (0px)
-
     //listen for mouseenter and pause
     //resume on mouseleave
+    $slider.on('mouseenter', pauseSlider).on('mouseleave', startSlider)
+    startSlider();
+
+
+    //Ajax
+
+    var $pokemon = $('#pokemon');
+    var $name = $('#name');
+    $('#add-pokemon').on('click', function () {
+        $.ajax({
+            type: 'GET',
+            url: `https://pokeapi.co/api/v2/pokemon/${$name.val()}`,
+            success: function (pokemon) {
+                $pokemon.append(`<li>name: ${pokemon.name} <img src="${pokemon.sprites.front_default}"> </li>`)
+                $pokemon.children().children('img').css({ height: '150px', width: '150px' })
+                $name.val('')
+            },
+            error: function () {
+                alert('error, non-existent pokemon');
+            }
+        });
+    });
 });
